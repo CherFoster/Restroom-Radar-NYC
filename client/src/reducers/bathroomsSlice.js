cd1b9101b9ef198a9d14b82f505c96c6997953bd
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-10-30 23:29:42 -0400

    updating reducers

--- client/src/reducers/bathroomsSlice.js
+++ client/src/reducers/bathroomsSlice.js
@@ -1,28 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = []
+// const initialState = []
 
 const bathroomsSlice = createSlice({
     name: 'bathrooms',
-    initialState,
+    initialState: {bathrooms: []},
     
     reducers: {
       // Give case reducers meaningful past-tense "event"-style names
-      bathroomAdded(state, action) {
-        const { id, text } = action.payload
-        // "Mutating" update syntax thanks to Immer, and no `return` needed
-        state.todos.push({
-          id,
-          text,
-          completed: false
-        })
+      setBathrooms: (state, action) =>{
+        state.bathrooms = action.payload
       }
     }
   })
   
   // `createSlice` automatically generated action creators with these names.
   // export them as named exports from this "slice" file to be used in components
-  export const { bathroomAdded } = bathroomsSlice.actions
+  export const { setBathrooms } = bathroomsSlice.actions
   
   // Export the slice reducer as the default export to be added to store
   export default bathroomsSlice.reducer
