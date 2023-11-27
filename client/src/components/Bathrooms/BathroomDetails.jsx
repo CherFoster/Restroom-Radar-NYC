67cf5532b46ec3162c049fa0421f9ca425a9d662
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-11-26 20:40:18 -0500

    updates

--- client/src/components/Bathrooms/BathroomDetails.jsx
+++ client/src/components/Bathrooms/BathroomDetails.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
 
 function BathroomDetails() {
+  const bathrooms = useSelector(state => state.bathrooms.bathrooms)
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    fetch(`/api/bathrooms/${id}`)
+    .then(res => res.json())
+    .then(data => {
+      dispatch(setBathrooms(data))
+    })
+  },[id])
+
+// do i need state?
   return (
     <div>BathroomDetails</div>
   )
--- client/src/reducers/bathroomsSlice.js
+++ client/src/reducers/bathroomsSlice.js
@@ -8,13 +8,17 @@ const bathroomsSlice = createSlice({
       // Give case reducers meaningful past-tense "event"-style names
       setBathrooms: (state, action) =>{
         state.bathrooms = action.payload
+      },
+
+      addBathrooms: (state, action) =>{
+        state.bathrooms = action.payload
       }
     }
   })
   
   // `createSlice` automatically generated action creators with these names.
   // export them as named exports from this "slice" file to be used in components
-  export const { setBathrooms } = bathroomsSlice.actions
+  export const { setBathrooms, addBathrooms } = bathroomsSlice.actions
   
   // Export the slice reducer as the default export to be added to store
   export default bathroomsSlice.reducer
