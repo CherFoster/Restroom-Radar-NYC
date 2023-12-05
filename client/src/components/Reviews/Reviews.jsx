c78b3426f9fe27f8495c7aabda360e835115ce0a
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-12-05 13:32:49 -0500

    adding reviews

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -1,15 +1,88 @@
 import React from 'react';
+import { useFormik } from 'formik';
 import { useDispatch, useSelector } from "react-redux";
 import { setReviews } from "../../reducers/reviewsSlice";
-
 
 function Reviews() {
   const reviews = useSelector(state => state.reviews.reviews)
   const dispatch = useDispatch()
+  
+  const handleDelete = (id) => {
+    // Add your delete logic here using the review id
+    console.log(`Deleting review with ID: ${id}`);
+  };
+
+  const handleEdit = (id) => {
+    // Add your edit logic here using the review id
+    console.log(`Editing review with ID: ${id}`);
+  };
+
+  const initialValues = {
+    reviewContent: '',
+  };
+
+  const onSubmit = (values, { resetForm }) => {
+    // Handle the submission of the review content here
+    console.log('Review submitted:', values.reviewContent);
+    resetForm();
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  });
 
   return (
-    <div>Reviews</div>
-  )
+    <div className="reviews">
+      <h3>REVIEWS:</h3>
+      <form className="review-form" onSubmit={formik.handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="reviewContent">Add a Review:</label>
+          <input
+            id="reviewContent"
+            name="reviewContent"
+            type="text"
+            className="form-control"
+            onChange={formik.handleChange}
+            value={formik.values.reviewContent}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+      <ul className="review-list">
+        {reviews.map((review) => (
+          <li key={review.id} className="review-item">
+            {review.content}
+            <button onClick={() => handleDelete(review.id)} className="btn btn-danger">Delete</button>
+            <button onClick={() => handleEdit(review.id)} className="btn btn-secondary">Edit</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Reviews
+export default Reviews;
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { useDispatch, useSelector } from "react-redux";
+// import { setReviews } from "../../reducers/reviewsSlice";
+
+
+// function Reviews() {
+//   const reviews = useSelector(state => state.reviews.reviews)
+//   const dispatch = useDispatch()
+
+//   return (
+//     <div>Reviews</div>
+//   )
+// }
+
+// export default Reviews
