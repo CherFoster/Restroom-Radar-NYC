f387bc535a2efd310aea1dcce31c9a91788b09d9
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-12-08 10:09:50 -0500

    updates

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -107,25 +107,3 @@ function Reviews({bathroomId}) {
 
 export default Reviews;
 
-
-
-
-
-
-
-
-// import React from 'react';
-// import { useDispatch, useSelector } from "react-redux";
-// import { setReviews } from "../../reducers/reviewsSlice";
-
-
-// function Reviews() {
-//   const reviews = useSelector(state => state.reviews.reviews)
-//   const dispatch = useDispatch()
-
-//   return (
-//     <div>Reviews</div>
-//   )
-// }
-
-// export default Reviews
