92e377a7724d09a301e4a3a1e22b4e3e999571f0
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-12-15 21:20:31 -0500

    updates

--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -1,34 +1,81 @@
-import React , {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from "react-redux";
-// import { login, logout } from '../../reducers/sessionSlice';
+import { useSelector } from 'react-redux';
 
 function User() {
-  const { loggedIn, currentUser } = useSelector(store => store.session);
-  const dispatch = useDispatch();
+  const { loggedIn, currentUser } = useSelector(store => store.auth);
   const navigate = useNavigate();
-  // const { currentUser, loggedIn } = useContext(UserContext);
-
+  const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
-    if(!loggedIn) {
-      navigate("/login")
+    if (!loggedIn) {
+      navigate('/user');
+    } else {
+      // Fetch reviews for the logged-in user
+      fetch(`/api/reviews/${currentUser.id}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`Failed to fetch reviews: ${response.statusText}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          setReviews(data); 
+        })
+        .catch(error => {
+          console.error(error);
+        });
     }
-  }, [loggedIn, navigate])
+  }, [loggedIn, navigate, currentUser.id]);
 
-  if(!loggedIn){ return <p>redirecting...</p> }
+  const handleCreateReview = async () => {
+    try {
+      const response = await fetch('/api/reviews', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user_id: currentUser,  // expects a user_id
+        }),
+      });
+      if (!response.ok) {
+        throw new Error(`Failed to create review: ${response.statusText}`);
+      }
 
-  const reviewCards = currentUser.reviews
-  
+      //show newly created review
+      const newReview = await response.json();
+
+      // Update the state to include the new review created by user
+      setReviews([...reviews, newReview]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (!loggedIn) {
+    return <p>redirecting...</p>;
+  }
+
   return (
     <div>
       <h1>My Reviews</h1>
-      { reviewCards }
+      <button onClick={handleCreateReview}>Create Review</button>
+      {reviews.map(review => (
+        <div key={review.id}>
+          <p>{review.conent}</p>
+          <p>bahtroom name of review?</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default User
+export default User;
+
+
+
+
 
 /*
 I need user to log in and see the reviews they've created... many user will use many bathrooms
